70c7e2a267331fc4d4a61ddf6d43169ddf2c58d4
Hitesh Mahto <hiteshmahto786@gmail.com>
2024-07-06 14:45:18 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
+import Player from "./components/Player";
+import Display from "./components/Display";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
+        <Sidebar />
+        <Display />
+      </div>
+      <Player />
+    </div>
+  );
 };
 
 export default App;
