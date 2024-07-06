adc7a562d1475e07e168f3c1f0bf2f20ccd3f8c4
Hitesh Mahto <hiteshmahto786@gmail.com>
2024-07-06 19:01:55 +0530

    done

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { useParams } from "react-router-dom";
 import { albumsData, assets, songsData } from "../assets/assets";
+import { PlayerContext } from "../context/PlayerContext";
 
 const DisplayAlbum = () => {
   const { id } = useParams();
   const albumData = albumsData[id];
+  const { playWithId } = useContext(PlayerContext);
   return (
     <>
       <Navbar />
@@ -28,28 +30,29 @@ const DisplayAlbum = () => {
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
+      <div className="grid grid-cols-2 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]">
         <p>
           <b className="mr-4">#</b>Title
         </p>
-        <p>Album</p>
+        <p className="hidden sm:block">Album</p>
         <p className="hidden sm:block">Date Added</p>
         <img className="m-auto w-4" src={assets.clock_icon} alt="" />
       </div>
       <hr />
       {songsData.map((item, index) => (
         <div
+          onClick={() => playWithId(item.id)}
           key={index}
-          className="grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
+          className="grid grid-cols-2 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer"
         >
-          <p className="text-white">
+          <p className="text-white w-60">
             <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
             <img className="inline w-10 mr-5" src={item.image} alt="" />
             {item.name}
           </p>
-          <p className="text-[15px]">{albumData.name}</p>
+          <p className="text-[15px] hidden sm:block">{albumData.name}</p>
           <p className="text-[15px] hidden sm:block">5 days ago</p>
-          <p>{item.duration}</p>
+          <p className="m-auto">{item.duration}</p>
         </div>
       ))}
     </>
